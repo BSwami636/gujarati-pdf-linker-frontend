fb14724728d992fb7a4ef5f7cdd1896c26d24aff
BSwami636 <timemassin@gmail.com>
2025-04-06 02:47:06 +0530

    Initial commit for frontend

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'Arial' }}>
+      <h1>Gujarati PDF Linker</h1>
+      <p>This is a simple React app to link PDF chapters.</p>
+    </div>
+  );
+}
+
+export default App;
